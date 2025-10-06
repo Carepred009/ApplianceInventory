3e5564b8825a425c379affa4fcb62cb56b27529f
Carepred <carepred.ballos88@gmail.com>
2025-10-06 20:57:16 +0800

    Redesign register.html. Remove class form control in forms.py for crispy forms.

--- InventoryProject/members/forms.py
+++ InventoryProject/members/forms.py
@@ -19,6 +19,18 @@ class UserProfile(forms.ModelForm):
 
         }
 
+#for crispy form
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput())
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+
+'''
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
@@ -34,3 +46,5 @@ class SignUpForm(UserCreationForm):
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+'''
